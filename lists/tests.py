8f8ba32d13d79b1e23c721628f92e1b047033d70
xuyan <informlarry@163.com>
2016-09-08 22:26:09 +0800

    5.2  Processing a POST Request on the Server

--- lists/tests.py
+++ lists/tests.py
@@ -16,4 +16,12 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('lists/home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(expected_html, response.content.decode())
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'lists/home.html')
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
+    else:
+        return render(request, 'lists/home.html')
